bf2948ea6cd56eb9657cbdfc3d87971275d37b5e
nekedio <nikitasi@tatmedia.com>
2022-06-29 10:48:59 +0300

    fixed errors

--- src/main/java/hexlet/code/schemas/MapSchema.java
+++ src/main/java/hexlet/code/schemas/MapSchema.java
@@ -23,15 +23,9 @@ public final class MapSchema implements BaseSchema {
         return this;
     }
 
-    public boolean isValid(Object map) {
-        if (!mapChecks.isEmpty()) {
-
-            return isValidForShare((Map<String, Object>) map);
-
-        }
-
+    public boolean isValid(Object value) {
         for (Rule check : checks) {
-            if (!check.isValid(map)) {
+            if (!check.isValid(value)) {
                 return false;
             }
         }
@@ -39,7 +33,11 @@ public final class MapSchema implements BaseSchema {
         return true;
     }
 
-    public boolean isValidForShare(Map<String, Object> map) {
+    public boolean isValid(Map<String, Object> map) {
+        if (map == null) {
+            return isValid((Object) map);
+        }
+
         for (String key : map.keySet()) {
             BaseSchema schema = mapChecks.get(key);
             Object value = map.get(key);
@@ -48,47 +46,13 @@ public final class MapSchema implements BaseSchema {
                 return false;
             }
         }
+
         return true;
     }
-
-
-//    public boolean isValid(NullType value) {
-//        System.out.println("++++++++++++++++++++++++++++++");
-//        return false;
-//    }
-
-//    public boolean isValid(Map<String, Object> map) {
-////        if (map == null) {
-////            return false;
-////        }
-//        System.out.println("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-//        for (String key : map.keySet()) {
-////            System.out.println(map.get(key));
-////            System.out.println(mapChecks.get(key).isValid(map.get(key)));
-//            BaseSchema schema = mapChecks.get(key);
-//            Object value = map.get(key);
-//            System.out.println("---------------");
-//            System.out.println(schema);
-//            System.out.println(value);
-////            System.out.println(schema.isValid(value));
-//            System.out.println("---------------");
-////            System.out.println(schema.isValid());
-////            System.out.println(mapChecks.get(key));
-////            System.out.println(value.isValid());
-//            if (!schema.isValid(value)) {
-//                return false;
-//            }
-//
-//        }
-//
-//        return true;
-//    }
 
     public void shape(Map<String, BaseSchema> schemas) {
         for (String key : schemas.keySet()) {
             mapChecks.put(key, schemas.get(key));
         }
-
-//        System.out.println(mapChecks.toString());
     }
 }
--- src/main/java/hexlet/code/schemas/mapSchemaRules/Required.java
+++ src/main/java/hexlet/code/schemas/mapSchemaRules/Required.java
@@ -5,9 +5,6 @@ import hexlet.code.schemas.Rule;
 public final class Required implements Rule {
     @Override
     public boolean isValid(Object value) {
-        if (value == null) {
-            return false;
-        }
-        return true;
+        return value != null;
     }
 }
